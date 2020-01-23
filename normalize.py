9d347a596e5cd243b2a3a4495bdbdd7dbec463d1
Martin Gerstmaier <Gerstmaier.Martin@gmx.net>
2020-01-23 15:53:29 +0100

    fix normalization so it works for all states and fills up empty columns of the School class

--- normalize.py
+++ normalize.py
@@ -27,25 +27,25 @@ School = namedtuple('School',
 
 
 def normalize(state):
-    with open(os.path.join(BASE_PATH, 'data/{}.json'.format(state))) as f:
+    with open(os.path.join(BASE_PATH, 'data/{}.json'.format(state)), encoding="utf8") as f:
         data = json.load(f)
-    with open(os.path.join(BASE_PATH, 'data/{}.csv'.format(state)), 'w', newline='') as f:
+    with open(os.path.join(BASE_PATH, 'data/{}.csv'.format(state)), 'w', newline='', encoding="utf8") as f:
         output = csv.writer(f)
         output.writerow(School._fields)
 
         for row in data:
             s = School(
-                id=row['info']['id'],
+                id=row['info']['id'] if 'id' in row['info'] else '',
                 name=row['info']['name'],
                 address=row['info']['address'],
                 address2='',
                 zip=row['info'].get('zip'),
                 city=row['info'].get('city'),
-                school_type=row['info']['school_type'],
+                school_type=row['info']['school_type'] if 'school_type' in row['info'] else '',
                 phone=row['info']['phone'],
-                fax=row['info']['fax'],
-                email=row['info']['email'],
-                website=row['info']['website']
+                fax=row['info']['fax'] if 'fax' in row['info'] else '',
+                email=row['info']['email'] if 'email' in row['info'] else '',
+                website=row['info']['website'] if 'website' in row['info'] else '',
             )
 
             output.writerow(s)
@@ -55,7 +55,7 @@ def normalize_mv():
     state = 'mecklenburg-vorpommern'
     with open(os.path.join(BASE_PATH, 'data/{}.json'.format(state))) as f:
         data = json.load(f)
-    with open(os.path.join(BASE_PATH, 'data/{}.csv'.format(state)), 'w') as f:
+    with open(os.path.join(BASE_PATH, 'data/{}.csv'.format(state)), 'w', newline='') as f:
         output = csv.writer(f)
         output.writerow(School._fields)
 
@@ -88,10 +88,44 @@ def normalize_mv():
             output.writerow(s)
 
 
+def normalize_nrw():
+    state = 'nrw'
+    with open(os.path.join(BASE_PATH, 'data/{}.json'.format(state))) as f:
+        data = json.load(f)
+    with open(os.path.join(BASE_PATH, 'data/{}.csv'.format(state)), 'w', newline='') as f:
+        output = csv.writer(f)
+        output.writerow(School._fields)
+
+        for row in data:
+            s = School(
+                id='NRW-{}'.format(row['Schulnummer']),
+                name=row['Schulbezeichnung_1'],
+                address=row['Strasse'],
+                address2='',
+                zip=row['PLZ'],
+                city=row['Ort'],
+                school_type=row['Schulform'],
+                phone=row['Telefonvorwahl'] + row['Telefon'] if row['Telefonvorwahl'] != None and row['Telefon'] != None else '',
+                fax=row['Faxvorwahl'] + row['Fax'] if row['Faxvorwahl'] != None and row['Fax'] != None else '',
+                email=row['E-Mail'],
+                website=row['Homepage']
+            )
+            output.writerow(s)
+
+
 if __name__ == '__main__':
-    normalize('brandenburg')
-    normalize('niedersachsen')
-    normalize('thueringen')
-    normalize('saarland')
-    normalize('schleswig-holstein')
-    normalize_mv()
+    # normalize('baden-württemberg')
+    # normalize('bayern')
+    # normalize('berlin')
+    # normalize('brandenburg')
+    # normalize('bremen')
+    # normalize('hamburg')
+    # normalize('niedersachsen')
+    # normalize_nrw()
+    # normalize('rheinland-pfalz')
+    # normalize('saarland')
+    # normalize('sachsen')
+    # normalize('sachsen-anhalt')
+    # normalize('schleswig-holstein')
+    # normalize('thueringen')
+    # normalize_mv()
